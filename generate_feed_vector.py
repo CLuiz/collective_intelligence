064b495c3b050f82a7ff1152570b4f01ff4628eb
Chris Luiz <chris@chrisluiz.io>
2017-07-15 22:27:58 -0400

    clusters

--- generate_feed_vector.py
+++ generate_feed_vector.py
@@ -49,3 +49,22 @@ if __name__ == '__main':
                 apcount.setdefault(word, 0)
                 if count > 1:
                     apcount[word] += 1
+    wordlist = []
+    for w, bc in apcount.items():
+        frac = float(bc) / len(feedlist)
+        if frac > 0.1 and frac < 0.5:
+            wordlist.append(w)
+
+    out = file('blogdata.txt', 'w')
+    out.write('Blog')
+    for word in wordlist:
+        out.write('\t{word}')
+        out.write('\n')
+    for blog, wc in wordcounts.items():
+        out.write(blog)
+        for word in wordlist:
+            if word in wc:
+                out.write('\t{wc[word]}')
+            else:
+                out.write('\t0')
+        out.write('\n')
