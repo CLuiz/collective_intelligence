548b51c7f0e1fbb78377bc842f62803158eff8dc
Chris Luiz <chris@chrisluiz.io>
2017-07-17 05:37:16 -0400

    clsutering helper functions

--- clusters.py
+++ clusters.py
@@ -1,4 +1,6 @@
 from math import sqrt
+from PIL import Image, ImageDraw
+
 
 def readfile(filename):
     with open(filename) as f:
@@ -18,7 +20,18 @@ def readfile(filename):
 
 
 def pearson(v1, v2):
-    
+    sum1 = sum(v1)
+    sum2 = sum(v2)
+
+    sum1_sq = sum([v ** 2 for v in v1])
+    sum2_sq = sum([v ** 2 for v in v2])
+
+    prod_sum = sum([v1[i] * v2[i] for i in range(len(v1))])
+
+    n = prod_sum - (sum1 * sum2 / len(v1))
+    den = sqrt((sum1_sq - (sum1 ** 2) / len(v1)) *
+               (sum2_sq - (sum2 ** 2) / len(v1)))
+    return 1.0 - n / den
 
 
 class BiCluster(object):
@@ -72,3 +85,42 @@ def hcluster(rows, distance=pearson):
         clust.append(new_cluster)
 
     return clust[0]
+
+
+def print_clust(clust, labels=None, n=0):
+    # indent to make a hierarchy layout
+    for i in range(n):
+        print(' ')
+    if clust.id < 0:
+        # neg id means this si a branch
+        print('-')
+    else:
+        # pos id means this is an endpoint
+        if labels is None:
+            print(clust.id)
+        else:
+            print(labels[clust.id])
+
+    if clust.left is not None:
+        print_clust(clust.left, labels=labels, n=n+1)
+    if clust.right is not None:
+        print_clust(clust.right, labels=labels, n=n+1)
+    return True
+
+
+def get_height(clust):
+    # Is this the endpoint? Then the hight is 1
+    if clust.left is None and clust.right is None:
+        return 1
+
+    # Otherwise the height is the same of the heights of each branch
+    return get_height(clust.left) + get_height(clust.right)
+
+
+def get_depth(clust):
+    # Distnace to endpoint is 0
+    if clust.left is None and clust.right is None:
+        return 0
+    # The distance of a branch is the greater of its two sides
+    # plus its own distance
+    return max(get_depth(clust.left), get_depth(clust.right)) + clust.distance
