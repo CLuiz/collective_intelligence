966c1509398b15b57cdf1a7232f66651bd19ea0f
Chris Luiz <chris@chrisluiz.io>
2017-07-18 06:18:48 -0400

    hclust helper funcs & kmeans

--- clusters.py
+++ clusters.py
@@ -1,3 +1,4 @@
+import random
 from math import sqrt
 from PIL import Image, ImageDraw
 
@@ -124,3 +125,107 @@ def get_depth(clust):
     # The distance of a branch is the greater of its two sides
     # plus its own distance
     return max(get_depth(clust.left), get_depth(clust.right)) + clust.distance
+
+
+def draw_dendrogram(clust, labels, jpeg='clusers.jpeg'):
+    h = get_height(clust) * 20
+    w = 1200
+    depth = get_depth(clust)
+
+    # Width is fixed, scale distances accordingly
+    scaling = float(w - 150) / depth
+
+    # Create new image with white background
+    img = Image.new('RGB', (w, h), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+
+    draw.line((0, h/2, 10, h/2), fill=(255, 0, 0))
+
+    # Draw first node
+    draw_node(draw, clust, 10 (h / 2), scaling, labels)
+    img.save(jpeg, 'JPEG')
+
+    return True
+
+
+def draw_node(draw, clust, x, y, scaling, labels):
+    if clust.id < 0:
+        h1 = get_height(clust.left) * 20
+        h2 = get_height(clust.right) * 20
+        top = y - (h1 + h2) / 2
+        bottom = y + (h1 + h2) / 2
+
+        # Line length
+        ll = clust.distance * scaling
+        # Vertical line from this cluster to children
+        draw.line((x, top+h1/2, x, bottom-h2/2),
+                  fill=(255, 255, 255))
+
+        # Horizonal line to left item
+        draw.line((x, top+h1/2, x+ll, top+h2/2),
+                  fill=(255, 255, 255))
+
+        # Horizonal line to right item
+        draw.line((x, bottom-h1/2, x+ll, bottom-h2/2),
+                  fill=(255, 255, 255))
+
+    # Call the fucntion to draw the left and right nodes
+        draw_node(draw, clust.left, x+11, top+h1/2, scaling, labels)
+        draw_node(draw, clust.right, x+11, bottom-h1/2, scaling, labels)
+    else:
+        # If this is an endpoint draw an item label
+        draw.text((x+5, y-7), labels[clust.id], (0, 0, 0))
+
+    return True
+
+
+def rotate_matrix(data):
+    new_data = []
+    for i in range(len(data[0])):
+        new_row = [data[j][i] for j in range(len(data))]
+        new_data.append(new_row)
+    return new_data
+
+
+def kmeans(rows, distance=pearson, k=4):
+    # Min and max values for each point
+    ranges = [(min([row[i] for row in rows]),
+               max([row[i] for row in rows]))
+              for i in range(len(rows[0]))]
+    # Create k randomly palced centroids
+    clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0]
+                 for i in range(len(rows[0]))]
+                for j in range(k)]
+
+    last_matches = None
+    for t in range(100):
+        print(f'Iteration {t}')
+        best_matches = [[] for i in range(k)]
+
+        # Find nearest centroid
+        for j in range(len(rows)):
+            row = rows[j]
+            best_match = 0
+            for i in range(k):
+                d = distance(clusters[i], row)
+                if d < distance(clusters[best_match], row):
+                    best_match = i
+                best_matches.append(j)
+
+        # If results are the same as last time, complete
+        if best_matches == last_matches:
+            break
+        last_matches = best_matches
+
+    # Move sentroids to the average of their members
+    for i in range(k):
+        avgs = [0.0] * len(rows[0])
+        if len(best_matches[i]) > 0:
+            for rowid in best_matches:
+                for m in range(len(rows[rowid])):
+                    avgs[m] += rows[rowid][m]
+            for j in range(len(avgs)):
+                avgs[j] /= len(best_matches[i])
+            clusters[i] = avgs
+
+    return best_matches
