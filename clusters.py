22ad6bb5939909c4382243541d69b5485036dde9
Chris Luiz <chris@chrisluiz.io>
2017-07-20 05:54:10 -0400

    scale_down function

--- clusters.py
+++ clusters.py
@@ -243,4 +243,65 @@ def tanamoto(v1, v2):
             shr += 1
 
     return 1.0 - (float(shr) / (c1 + c2 - shr))
-    
+
+
+def scale_down(data, distance=pearson, rate=0.01):
+    n = len(data)
+    # calculate real distances between the points
+    real_dist = [[distance(data[i], data[j])
+                 for j in range(n)]
+                 for i in range(n)]
+    # outer_sum = 0.0
+
+    # Randomly initialize the starting points of the locationsin 2d
+    loc = [[random.random(), random.random()] for i in range(n)]
+    fake_dist = [[0.0 for j in range(n)]
+                 for i in range(n)]
+
+    last_error = None
+    for m in range(0, 1000):
+        for i in range(n):
+            for j in range(n):
+                fake_dist[i][j] = sqrt(sum([(loc[i][x] - loc[j][x]) ** 2
+                                       for x in range(len(loc[i]))]))
+        # move points
+        grad = [[0.0, 0.0] for i in range(n)]
+
+        total_error = 0
+        for k in range(n):
+            for j in range(n):
+                if j != k:
+                    # error is the percent difference between distances
+                    error_term = (fake_dist[j][k] - real_dist[j][k] /
+                                  real_dist[j][k])
+
+                    # each point needs to be moved from or towards th other
+                    # point in proportion to error
+                    grad[k][0] += ((loc[k][0] - loc[j][0]) /
+                                   fake_dist[j][k] * error_term)
+                    grad[k][1] += ((loc[k][1] - loc[j][1]) /
+                                   fake_dist[j][k] * error_term)
+                    total_error += abs(error_term)
+        print(error_term)
+
+        # If the last answer got worse by moving the points, we are done
+        if last_error and last_error > total_error:
+            break
+        last_error = total_error
+
+        # Move each point by thte elarning rate times the gradient
+        for k in range(n):
+            loc[k][0] -= rate * grad[k][0]
+            loc[k][1] -= rate * grad[k][1]
+    return loc
+
+
+def draw2(data, labels, jpeg='mds3d.jpg'):
+    img = Image.new('RGB', (2000, 2000), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    for i in range(len(data)):
+        x = (data[i][0] + 0.5) * 1000
+        y = (data[i][1] + 0.5) * 1000
+        draw.text((x, y), labels[i], (0, 0, 0, 0))
+    img.save(jpeg, 'JPEG')
+    return True
